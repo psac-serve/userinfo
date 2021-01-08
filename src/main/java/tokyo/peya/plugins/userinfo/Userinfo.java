4f24cdd456426bf2070fffae511012babf44aa5c
peyang-Celeron <42040068+peyang-Celeron@users.noreply.github.com>
2021-01-09 08:42:23 +0900

    Initial commit

---
+++ src/main/java/tokyo/peya/plugins/userinfo/Userinfo.java
@@ -0,0 +1,20 @@
+package tokyo.peya.plugins.userinfo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Userinfo extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+        // Plugin shutdown logic
+    }
+}
